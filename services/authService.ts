a437f79f284a8eb5ac24f1f6e69b1d4516db0526
Akshita J Shah <akshitashah2106@gmail.com>
2026-03-16 21:27:09 +0530

    Enhance authService with persistence settings
    Added browserLocalPersistence and setPersistence to authService for managing user session persistence.

--- services/authService.ts
+++ services/authService.ts
@@ -4,7 +4,9 @@ import {
   GoogleAuthProvider, 
   signOut, 
   onAuthStateChanged,
-  User as FirebaseUser
+  User as FirebaseUser,
+  browserLocalPersistence,
+  setPersistence
 } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { User } from "../types";
@@ -14,8 +16,9 @@ const provider = new GoogleAuthProvider();
 
 export const authService = {
   async signInWithGoogle(): Promise<User> {
+    await setPersistence(auth, browserLocalPersistence);
     const result = await signInWithPopup(auth, provider);
-    const firebaseUser = result.user;
+    //const firebaseUser = result.user;
     
     // Check if user exists in Firestore
     try {
